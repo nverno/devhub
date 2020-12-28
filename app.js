349236875bdd47717d9d2d830b99e5b2e28161ed
nverno <noah.v.peart@gmail.com>
2020-12-27 17:14:12 -0800

    squash

--- app.js
+++ app.js
@@ -1,34 +1,33 @@
 const express = require('express');
 const app = express();
-const db = require("./config/keys").mongoURI;
+const db = require('./config/keys').mongoURI;
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const path = require('path');
 
-const users = require("./routes/api/users");
+const users = require('./routes/api/users');
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("frontend/build"));
-  app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   });
 }
 
 app.use(passport.initialize());
-require("./config/passport")(passport);
+require('./config/passport')(passport);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Routes
-app.get("/", (req, res) => { res.json({ msg: "There is no spoon"}); });
-app.use("/api/users", users);
-
+// app.get("/", (req, res) => { res.json({ msg: "There is no spoon"}); });
+app.use('/api/users', users);
 
 mongoose
   .connect(db, { useNewUrlParser: true })
-  .then(() => console.log("Connected to MongoDB successfully"))
+  .then(() => console.log('Connected to MongoDB successfully'))
   .catch((err) => console.log(err));
 
 const port = process.env.PORT || 5000;
